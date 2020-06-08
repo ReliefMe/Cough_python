b49d7c99540291942171556c15c7c1da5894c219
furqan4545 <furqan.ali9500@gmail.com>
2020-06-08 07:06:47 +0500

    ds

--- application.py
+++ application.py
@@ -129,7 +129,7 @@ def data():
 #     app.run(debug=True)
     
 
-if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=80)
+# if __name__ == '__main__':
+#     application.run(host='0.0.0.0', port=80)
 
 
